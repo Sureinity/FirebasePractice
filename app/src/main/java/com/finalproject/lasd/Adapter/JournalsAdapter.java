2136382eef42cae0a7a156c57adcb05415bfeee0
Sureinity <johnghlendealdo@gmail.com>
2024-05-02 14:09:43 +0800

    Second Commit

--- app/src/main/java/com/finalproject/lasd/Adapter/JournalsAdapter.java
+++ app/src/main/java/com/finalproject/lasd/Adapter/JournalsAdapter.java
@@ -1,38 +1,62 @@
 package com.finalproject.lasd.Adapter;
 
-import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class JournalsAdapter extends RecyclerView.Adapter<JournalsAdapter.MyViewHolder> {
+import com.finalproject.lasd.R;
+import com.google.firebase.database.DataSnapshot;
 
-    Context context;
+public class JournalsAdapter extends RecyclerView.Adapter<JournalsAdapter.TitleDescriptionViewHolder> {
 
+    private DataSnapshot dataSnapshot;
+
+    public JournalsAdapter(DataSnapshot dataSnapshot) {
+        this.dataSnapshot = dataSnapshot;
+    }
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public TitleDescriptionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.journals_item, parent, false);
+        return new TitleDescriptionViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull TitleDescriptionViewHolder holder, int position) {
+        if (dataSnapshot.getChildrenCount() == 0) {
+            // Handle the case where there are no journal items
+            return;
+        }
 
+        DataSnapshot journalSnapshot = dataSnapshot.getChildren().iterator().next();
+
+        String title = journalSnapshot.child("title").getValue(String.class);
+        String description = journalSnapshot.child("description").getValue(String.class);
+
+        holder.titleTextView.setText(title);
+        holder.descriptionTextView.setText(description);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return (int) dataSnapshot.getChildrenCount();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
+    public static class TitleDescriptionViewHolder extends RecyclerView.ViewHolder {
 
-        public MyViewHolder(@NonNull View itemView) {
+        TextView titleTextView;
+        TextView descriptionTextView;
+
+        public TitleDescriptionViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            titleTextView = itemView.findViewById(R.id.titleTextView);
+            descriptionTextView = itemView.findViewById(R.id.descriptionTextView);
         }
     }
-
 }
--- app/src/main/java/com/finalproject/lasd/MainActivity.java
+++ app/src/main/java/com/finalproject/lasd/MainActivity.java
@@ -2,24 +2,45 @@ package com.finalproject.lasd;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.finalproject.lasd.R;
+
+import com.finalproject.lasd.Adapter.JournalsAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private JournalsAdapter journalsAdapter;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recycler_view_story);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Journals");
+
+        journalsAdapter = new JournalsAdapter(databaseReference);
+        recyclerView.setAdapter(journalsAdapter);
     }
 
-    public void repFragment(){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        journalsAdapter.startListening();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        journalsAdapter.stopListening();
     }
 }
